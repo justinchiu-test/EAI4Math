b0df323fa4b282361bd76049f857012f13c75502
justinchiu-cohere <justinchiu@cohere.com>
2024-09-04 09:13:50 -0400

    curl albert's MMA dataset

---
+++ scripts/get_mma.sh
@@ -0,0 +1,9 @@
+curl -LJ https://github.com/albertqjiang/MMA/raw/main/data/MMA%20dataset.zip -o data/MMA.zip
+
+pushd data
+unzip MMA.zip
+mv "MMA dataset" MMA
+# cleanup
+rm -rf __MACOSX
+rm -rf data/MMA/__MACOSX
+popd
