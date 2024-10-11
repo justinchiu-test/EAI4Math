261d2d2c78c43618ceb31de2f6133c3fb892c31b
justinchiu-cohere <justinchiu@cohere.com>
2024-10-11 00:07:32 -0400

    feat: Add async support for querying Lean server and prover

--- autoformalization/src/autoformalization/minif2f.py
+++ autoformalization/src/autoformalization/minif2f.py
@@ -1,18 +1,36 @@
+import asyncio
 from datasets import load_dataset
 import requests
 from typing import List
 import json
 
 
-def query_lean_server(lean_codes: List[str]):
-    url = 'https://justinchiu--verifier-verify.modal.run/'
+async def query_prover(statements: List[str]):
+    url = 'https://justinchiu--deepseek-prover-web.modal.run'
     headers = {'Content-Type': 'application/json'}
-    data = [{"code": lean_code} for lean_code in lean_codes]
+    data = {
+        "prompts": statements,
+        "settings": {},
+    }
     data = json.dumps(data)
     
     response = requests.post(url, headers=headers, data=data)
 
     results = json.loads(response.text)
+    return results
+
+async def query_lean_server(lean_codes: List[str]):
+    url = 'https://justinchiu--verifier-verify.modal.run/'
+    headers = {'Content-Type': 'application/json'}
+    data = [{"code": lean_code.replace("```","").strip()} for lean_code in lean_codes]
+    data = json.dumps(data)
+    
+    response = requests.post(url, headers=headers, data=data)
+
+    try:
+        results = json.loads(response.text)
+    except:
+        import pdb; pdb.set_trace()
     return results
 
 def test_lean_server():
@@ -22,12 +40,25 @@ def test_lean_server():
 
 #test_lean_server()
 
-dataset = load_dataset("cat-searcher/minif2f-lean4")
 
-def test_minif2f_lean4():
-    sample = dataset["test"][0]
-    #results = query_lean_server([sample["header"]+"\n"+sample["formal_statement"]])
-    results = query_lean_server(["import Mathlib\n"+sample["formal_statement"]])
-    print(results)
-    
-test_minif2f_lean4()
+async def test_minif2f_lean4():
+    dataset = load_dataset("cat-searcher/minif2f-lean4")
+    samples = dataset["test"]
+    theorems_without_sorry = [
+        "Complete the following Lean 4 code:\n\n```lean4\n"
+        + sample["formal_statement"].replace(" sorry", "")
+        for sample in samples
+    ]
+    generations = query_prover(theorems_without_sorry)
+    full_programs = [
+        "import Mathlib\n" + theorem + generation[0]
+        for theorem, generation in zip(theorems_without_sorry, generations)
+    ]
+    results = [
+        query_lean_server([program])
+        for program in full_programs
+    ]
+    import pdb; pdb.set_trace()
+
+if __name__ == "__main__":
+    asyncio.run(test_minif2f_lean4())
